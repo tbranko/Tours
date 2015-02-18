6f38d5f47bcc4154d5037778d411de309bf0c702
Branko Trifunovic <tbranko@gmail.com>
2015-02-18 11:45:31 +0100

    pre pokusaja da se doda gps

--- app/src/main/java/com/railzapp/tours/CameraPreview.java
+++ app/src/main/java/com/railzapp/tours/CameraPreview.java
@@ -37,6 +37,9 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 
     public void surfaceDestroyed(SurfaceHolder holder) {
         // empty. Take care of releasing the Camera preview in your activity.
+        this.getHolder().removeCallback(this);
+        mCamera.stopPreview();
+        mCamera.release();
     }
 
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
--- app/src/main/java/com/railzapp/tours/MainActivity.java
+++ app/src/main/java/com/railzapp/tours/MainActivity.java
@@ -1,12 +1,16 @@
 package com.railzapp.tours;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.hardware.Camera;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.FrameLayout;
@@ -17,6 +21,9 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     // define the display assembly compass picture
     private ImageView image;
+
+    // GPS class is helper for all Location based stuff
+    private GPS gps;
 
     // record the compass picture angle turned
     private float currentDegree = 0f;
@@ -34,6 +41,8 @@ public class MainActivity extends Activity implements SensorEventListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        gps = new GPS(this);
+
         //
         image = (ImageView) findViewById(R.id.imageViewCompass);
 
@@ -42,20 +51,23 @@ public class MainActivity extends Activity implements SensorEventListener {
 
         // initialize your android device sensor capabilities
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-
-        // Create an instance of Camera
-        mCamera = getCameraInstance();
-        mCamera.setDisplayOrientation(90);
-
-        // Create our Preview view and set it as the content of our activity.
-        mPreview = new CameraPreview(this, mCamera);
-        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-        preview.addView(mPreview);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+
+        if (mCamera == null) {
+            mCamera = getCameraInstance();
+            mCamera.setDisplayOrientation(90);
+
+           // TODO For orientation, check http://stackoverflow.com/questions/20800630/java-lang-runtimeexception-method-called-after-release
+
+            // Create our Preview view and set it as the content of our activity.
+            mPreview = new CameraPreview(this, mCamera);
+            FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+            preview.addView(mPreview);
+        }
 
         // for the system's orientation sensor registered listeners
         mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
@@ -68,6 +80,30 @@ public class MainActivity extends Activity implements SensorEventListener {
 
         // to stop the listener and save battery
         mSensorManager.unregisterListener(this);
+
+        if (mCamera != null) {
+            mCamera = null;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_activity_actions, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.map:
+                Intent intent_home_location = new Intent(this, MapsActivity.class);
+                startActivity(intent_home_location);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
@@ -75,6 +111,13 @@ public class MainActivity extends Activity implements SensorEventListener {
 
         // get the angle around the z-axis rotated
         float degree = Math.round(event.values[0]);
+
+//        gps.updateGPSCoordinates();
+//        Location currentLoc = gps.getLocation();
+//        Location destination = new Location("");
+//        destination.setLatitude(50.8319238);
+//        destination.setLongitude(4.3623699);
+//        degree = currentLoc.bearingTo(destination);
 
         tvHeading.setText("Heading: " + Float.toString(degree) + " degrees");
 
